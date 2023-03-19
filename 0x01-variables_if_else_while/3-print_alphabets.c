0477ebfa97ef5a7d91b4405dee5d63f47faef8bc
Ma-rwan <marwaneltaieby@hitmail.com>
2023-03-19 17:19:46 +0000

    u & l Case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success0
+ */
+int main(void)
+{
+	int l = 97;
+	int u = 65;
+
+	while (l <= 122)
+	{
+		putchar(l);
+		l++;
+	}
+	while (u <= 90)
+	{
+		putchar(u);
+		u++;
+	}
+	putchar('\n');
+	return (0);
+}
