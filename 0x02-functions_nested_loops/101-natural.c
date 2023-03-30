46e01978f7883bf16429981d7051ab29719be54b
Ma-rwan <marwaneltaieby@hitmail.com>
2023-03-30 10:19:16 +0000

    sum num

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	in sum, num;
+	int sum, num;
 
 	for (num = 0; num < 1024; num++)
 	{
 		if ((num % 3 == 0) || (num % 5 == 0))
 			sum += num;
 	}
-	printf("d\n", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
