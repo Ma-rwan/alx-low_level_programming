ffbb3905976ff2e8cad832bbb84a697ac7ca5e57
Ma-rwan <marwaneltaieby@hitmail.com>
2023-03-30 21:40:41 +0000

    fibonacci 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * numLength - entry point
  * @num: operand number
  * Return: num of digits
  */
@@ -29,7 +29,7 @@ int numLength(int num)
 int main(void)
 {
 	int count, initial0s;
-	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o= 0, sumo = 0;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
 	for (count = 1; count <= 98; count++)
 	{
@@ -45,9 +45,10 @@ int main(void)
 
 		printf("%lu", f1);
 
-		sum =(f1 + f2) % mx;
-		sumo = f1o + f2o + (f1 +f2) / mx;
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
 		f1 = f2;
+		f1o = f2o;
 		f2 = sum;
 		f2o = sumo;
 
