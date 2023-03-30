5dbeaba1e3ebeec4afe50bd3be64258d8e588c79
Ma-rwan <marwaneltaieby@hitmail.com>
2023-03-30 08:25:12 +0000

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,7 +13,8 @@ int print_last_digit(int n)
 	if (n < 0)
 		lastDigit = -1 * (n % 10);
 	else
-		lastDigit = n & 10;
+		lastDigit = n % 10;
+
 	_putchar(lastDigit + '0');
 	return (lastDigit);
-}	
+}
