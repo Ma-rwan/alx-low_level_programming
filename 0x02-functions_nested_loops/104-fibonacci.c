9fab38aa416853b2cbeeca1e61fda9ff4d867fee
Ma-rwan <marwaneltaieby@hitmail.com>
2023-03-30 21:25:55 +0000

    fibonacci 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,61 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ * @num: operand number
+ * Return: num of digits
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (leght);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f10 = 0, f2o= 0, sumo = 0;
+
+	for (count = 1; count <= 98; count++)
+	{
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLenght(f1);
+
+		while (f1 > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			initial0s--;
+		}
+
+		printf("%lu", f1);
+
+		sum =(f1 + f2) % mx;
+		sumou = f1o + f2o + (f1 +f2) / mx;
+		f1 = f2;
+		f2 = sum;
+		f2o = sumo;
+
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
