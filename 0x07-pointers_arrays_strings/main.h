8b84cabf5d0d01b37baa79178c2ee8bb3ff54b88
Ma-rwan <marwaneltaieby@hitmail.com>
2023-04-04 08:51:04 +0000

    07 pointer & arrays

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-har *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
@@ -16,7 +16,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 
 void print_diagsums(int *a, int size);
-Format: see example
 
 
 
