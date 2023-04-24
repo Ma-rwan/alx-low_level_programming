9882589b9b878cad77b7a0d99a38c0137f98f2f8
Ma-rwan <marwaneltaieby@hitmail.com>
2023-04-24 09:48:24 +0000

    more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (new_size > old_size)
-        {
+	{
 		p = malloc(new_size);
 		if (p == NULL)
 			return (NULL);
