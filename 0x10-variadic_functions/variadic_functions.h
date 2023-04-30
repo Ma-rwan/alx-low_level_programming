fcb71bfff33516df17f4c9cc5d11d126148807ef
Ma-rwan <marwaneltaieby@hotmail.com>
2023-04-30 10:06:01 +0000

    VF

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,32 +1,8 @@
-#ifndef MAIN_HEADER
-#define MAIN_HEADER
+#ifndef VAR_H
+#define VAR_H
 
-#include <stdarg.h>
-#include <stdio.h>
 
-/**
- * struct token - Struct token
- *
- * @token: format token
- * @f: the fun associated
- */
-typedef struct token
-{
-	char *token;
-	void (*f)(char *, va_list);
-} token_t;
-
-/**
- * _putchar - writes the character c in stdout
- * @c: the character to print]
- *
- * Return: on success 1.
- * on error, -1 is returned, and errno is set.
- */
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
-}
+int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
 
