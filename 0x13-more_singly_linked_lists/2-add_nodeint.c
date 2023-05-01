52842267f194301604691c12e203eb911403d41c
Ma_rwan <marwaneltaieby@hotmail.com>
2023-05-01 03:13:37 -0700

    more linked lists

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the start of a linked list
+ * @head: pointer
+ * @n: data to insert in the new node
+ * Return: pointer
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	list_y *new;
+
+	new = maloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
