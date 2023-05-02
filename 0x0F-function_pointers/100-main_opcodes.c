b892ba21f92f1158a8ed39e44ccca665ad528ad5
Ma_rwan <marwaneltaieby@hotmail.com>
2023-05-02 02:25:25 -0700

    function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -6,39 +6,23 @@
  * main - prints its own op codes
  * @argc: num of arg
  * @argv: array og arg
- * Return: Always 0 (Success0
+ * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int bytes, i;
-	char *arr;
+	int b;
+	char *p = (char *)main;
 
 	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
 
-	bytes = atoio(argv[1]);
+	if (b < 0)
 
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
+		printf("Error\n"), exit(2);
 
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-
-		printf("%02hhx", arr[i]);
-	}
+	while (n--)
+		printf("02hhx%s", *p++, b ? " " : "\n");
 	return (0);
 }
