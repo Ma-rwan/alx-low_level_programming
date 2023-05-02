e4fc10c9b0550456984eb3a25ee051b3bb3826ef
Ma_rwan <marwaneltaieby@hotmail.com>
2023-05-02 03:18:08 -0700

    liked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (!head || !new_node)
 		return (NULL);
 
-	new_node->next = null;
+	new_node->next = NULL;
 	new_node->n = n;
 	if (!*head)
 		*head = new_node;
