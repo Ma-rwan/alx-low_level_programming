46dc6b36287a1fb1e507fcbd8160b1cf18376d59
Ma_rwan <marwaneltaieby@hotmail.com>
2023-05-02 03:07:25 -0700

    function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <stdlib.h>
 
 /**
  * get_op_func - Selects the correct fub to perform
@@ -17,13 +16,17 @@ int(*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
+		{NULL, NULL}
 	};
 
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
+	{
+		if (s && s [0] == ops[i].op[0] && !s[1])
+			return (ops[i].f);
 		i++;
+	}
 
-	return (ops[i].f);
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include "3-calc.h"
 
 /**
  * main - {rints simple operations
@@ -10,34 +7,23 @@
  * Return: Always 0.
  */
 
-int main(int__attribute__((__unsed__)) argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int num1, num2;
-	char *op;
+	int (*op_func)(int, int), a, b;
 
 	if (argc != 4)
-	{
-		print("Error\n");
-		exit(98);
-	}
+		print("Error\n"), exit(98);
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+		printf("Error\n"), exit(99);
 
-	if ((*op == '/' && num2 == 0) ||
-			(*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	printf("%d\n", get_op_func(op)(num1, num2));
+	if (!b && (argv[2][0] == '/' || arvg[2][0] == '%'))
+		printf("Error\n"), exit(100);
+	printf("%d\n", op_func(op)(a, b));
 
 	return (0);
 }
