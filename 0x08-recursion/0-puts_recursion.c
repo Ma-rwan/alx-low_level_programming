ec34cd9d09565ded959233e976f91b1ec3948e2c
Ma_rwan <marwaneltaieby@hotmail.com>
2023-06-20 09:09:47 +0000

    q

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursions - prints a string, + new line
+ * @s: pointer to the string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
