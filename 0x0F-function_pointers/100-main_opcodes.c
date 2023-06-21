d9dce4aa711b6de4b00785c807e8f43e8bba4747
Ma_rwan <marwaneltaieby@hotmail.com>
2023-06-21 12:32:37 +0000

    .

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code for the school students.
+ * @argc: the number of args
+ * @argv: arguments victor
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+	printf("Error\n"), exit(2);
+
+	while (b--)
+		printf("02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
+
