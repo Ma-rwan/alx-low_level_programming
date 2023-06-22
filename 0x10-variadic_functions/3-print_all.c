a0e1840af39df4404aa3332af8b957b5c138d20a
Ma_rwan <marwaneltaieby@hotmail.com>
2023-06-22 11:37:23 +0000

    L

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,7 +19,7 @@ void format_char(char *separator, va_list ap)
 
 void format_int(char *separator, va_list ap)
 {
-        printf("%s%d", separator, va_arg(ap, int));
+	printf("%s%d", separator, va_arg(ap, int));
 }
 
 /**
@@ -30,7 +30,7 @@ void format_int(char *separator, va_list ap)
 
 void format_float(char *separator, va_list ap)
 {
-        printf("%s%f", separator, va_arg(ap, double));
+	printf("%s%f", separator, va_arg(ap, double));
 }
 
 /**
@@ -42,10 +42,11 @@ void format_float(char *separator, va_list ap)
 void format_string(char *separator, va_list ap)
 {
 	char *str = va_arg(ap, char *);
+
 	switch ((int)(!str))
 	case 1:
 		str = "(nil)";
-        printf("%s%s", separator, str);
+		printf("%s%s", separator, str);
 }
 
 /**
