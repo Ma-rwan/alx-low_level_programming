8fa12fae559dbfd30e776cf1c5297ff6f1dffb17
Ma-rwan <marwaneltaieby@hotmail.com>
2023-07-06 06:49:19 +0000

    I

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -38,7 +38,7 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i, num = 0;
-	const listint_ **list = NULL;
+	const listint_t **list = NULL;
 
 	while (head != NULL)
 	{
